06022fa00c2122d97c068c8ec4653292ee4ec07e
shishir <shisirsharma22@gmail.com>
2024-05-24 23:37:57 +0545

    added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,9 +13,11 @@ function App() {
         <h1 className="text-4xl font-bold text-blue-600">
           Hello, Tailwind CSS!
         </h1>
+        <Button>Hello</Button>
       </div>
     </>
   );
 }
 
 export default App;
+git
