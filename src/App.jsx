041d735408ec1ae14677d99360ad3850d6d3d831
shishir <shisirsharma22@gmail.com>
2024-05-24 23:21:00 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-blue-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind CSS!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
