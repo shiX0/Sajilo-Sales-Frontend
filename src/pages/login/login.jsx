84413724da420f244b1239b271361bccfdc4a652
shishir <shisirsharma22@gmail.com>
2024-05-27 23:27:01 +0545

    changed some design

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,46 +1,53 @@
 import React from "react";
+import logo from "@/assets/logo.svg";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import DarkModeToggle from "@/components/darkmodetoggle";
 
 const Login = () => {
   return (
-    <div className="min-h-screen flex">
-      <div className="w-1/2 bg-gray-900 flex flex-col justify-center items-center text-white">
-        <img
-          src="https://images.pexels.com/photos/7175967/pexels-photo-7175967.jpeg"
-          alt="Placeholder Image"
-          width={300}
-          height={300}
-          className="mb-8"
-        />
-        <p className="italic text-center">
-          “This library has saved me countless hours of work and helped me
-          deliver stunning designs to my clients faster than ever before.”
-        </p>
-        <p className="mt-4">Sofia Davis</p>
+    <>
+      <div className="flex h-screen w-screen">
+        <div className="w-1/2 bg-gray-900 flex flex-col justify-center items-center text-white">
+          <img
+            src="https://images.unsplash.com/photo-1602665742701-389671bc40c0?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9zfGVufDB8fDB8fHww"
+            alt="Placeholder Image"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute top-0 left-0 flex justify-center items-center text-white p-8">
+            <img src={logo} alt="logo" />
+            <h1 className="text-5xl font-bold ml-4 text-gray-700">
+              Sajilo Sales
+            </h1>
+          </div>
+        </div>
+        <div className="w-1/2 bg-black flex flex-col justify-center items-center">
+          <a className="mb-8" />
+          <h1 className="text-2xl mb-4 text-white">Login In with email</h1>
+          <form className="w-2/3">
+            <Input
+              type="email"
+              placeholder="name@example.com"
+              className="mb-4 text-white"
+            />
+
+            <Input
+              type="password"
+              placeholder="Enter your passsword"
+              className="mb-4 text-white"
+            />
+            <Button className="w-full mb-4">Log In</Button>
+            {/* Remove the GitHub Button */}
+          </form>
+          <div className="flex justify-around">
+            <Button variant="ghost">
+              <a href="/register">Register</a>
+            </Button>
+            <Button variant="ghost">Forgot Password?</Button>
+          </div>
+        </div>
       </div>
-      <div className="w-1/2 bg-black flex flex-col justify-center items-center">
-        <a className="mb-8" />
-        <h1 className="text-2xl mb-4 text-white">Create an account</h1>
-        <form className="w-2/3">
-          <Input type="email" placeholder="name@example.com" className="mb-4" />
-          <Button className="w-full mb-4">Sign In with Email</Button>
-          {/* Remove the GitHub Button */}
-        </form>
-        <p className="text-gray-500 text-sm">
-          By clicking continue, you agree to our
-          <a href="/terms" className="underline text-gray-300">
-            Terms of Service{" "}
-          </a>
-          and
-          <a href="/privacy" className="underline text-gray-300">
-            Privacy Policy
-          </a>
-          .
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
-
 export default Login;
