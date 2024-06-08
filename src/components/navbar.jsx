0e28963abb07a43167faa1ffde9c932c09f24d10
shishir <shisirsharma22@gmail.com>
2024-06-08 23:50:25 +0545

    Added navigation

---
+++ src/components/navbar.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import DarkModeToggle from "./darkmodetoggle";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <div className="flex mb-8 mx-auto border-gray-200 dark:border-gray-700 py-4 px-8 justify-between items-center">
+        <div>
+          <Button variant="link">
+            <NavLink
+              to="/"
+              exact
+              className={` hover:text-gray-200 ${
+                location.pathname === "/"
+                  ? "font-bold text-gray-200"
+                  : "text-gray-400"
+              }`}
+            >
+              Overview
+            </NavLink>
+          </Button>
+          <Button variant="link">
+            <NavLink
+              to="/Customers"
+              exact
+              className={` hover:text-gray-200 ${
+                location.pathname === "/Customers"
+                  ? "font-bold text-gray-200"
+                  : "text-gray-400"
+              }`}
+            >
+              Customers
+            </NavLink>
+          </Button>
+          <Button variant="link">
+            <NavLink
+              to="/Products"
+              exact
+              className={` hover:text-gray-200 ${
+                location.pathname === "/Products"
+                  ? "font-bold text-gray-200"
+                  : "text-gray-400"
+              }`}
+            >
+              Products
+            </NavLink>
+          </Button>
+          <Button variant="link">
+            <NavLink
+              to="/Orders"
+              exact
+              className={` hover:text-gray-200 ${
+                location.pathname === "/Orders"
+                  ? "font-bold text-gray-200"
+                  : "text-gray-400"
+              }`}
+            >
+              Orders
+            </NavLink>
+          </Button>
+          <Button variant="link">
+            <NavLink
+              to="/Settings"
+              exact
+              className={` hover:text-gray-200 ${
+                location.pathname === "/Settings   "
+                  ? "font-bold text-gray-200"
+                  : "text-gray-400"
+              }`}
+            >
+              Settings
+            </NavLink>
+          </Button>
+        </div>
+
+        <DarkModeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
